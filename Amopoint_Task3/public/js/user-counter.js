4a6dd06696023990e2e0d264565805e99334f67f
RAMIL <53734360+ramiltheking@users.noreply.github.com>
2025-02-16 15:23:04 +0500

    User stats update

--- Amopoint_Task3/public/js/user-counter.js
+++ Amopoint_Task3/public/js/user-counter.js
@@ -1,47 +1,29 @@
 const _token = document.querySelector(`meta[name="csrf-token"]`).getAttribute('content');
 
-
 document.addEventListener("DOMContentLoaded", function(){
-    fetch('https://api.ipify.org?format=json')
+    fetch('https://ipinfo.io/json') 
     .then(response => response.json())
     .then(data => {
         let ip = data.ip;
-        navigator.geolocation.getCurrentPosition(
-        position => {
-            let { latitude, longitude } = position.coords;
+        let city = data.city || "Неизвестно";
+        let device = navigator.userAgent;
 
-            fetch(`https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}`)
-            .then(
-                response => response.json()
-            )
-            .then(data => {
-                const city = data.address.city;
-                const device = navigator.userAgent;
-                fetch("/save-user-stats", {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({ ip, city, device, _token})
-                })
-                .then(response => response.json())
-                .then(data => {
-                  console.log('Данные отправлены:', data);
-                })
-                .catch(error => {
-                  console.error('Ошибка отправки данных в обработчик:', error);
-                });
-            })
-            .catch(error => {
-                console.error('Ошибка:', error);
-            });
-        },
-        error => {
-            console.error('Ошибка:', error);
-        }
-        );
+        fetch("/save-user-stats", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ip, city, device, _token })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Данные отправлены:', data);
+        })
+        .catch(error => {
+            console.error('Ошибка отправки данных в обработчик:', error);
+        });
     })
     .catch(error => {
-        console.error('Ошибка получения IP:', error);
+        console.error('Ошибка получения данных по IP:', error);
     });
-})
+});
